c30fe6268934e9581c178cc9244b191c18212a7b
Владислав Куцак <vladyslav.kutsak.knm.2018@lpnu.ua>
2022-01-13 19:58:02 +0200

    cesarian_code

---
+++ cesarean_code.rb
@@ -0,0 +1,20 @@
+def cesarean_code(text,key = 1)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  alphabet_up = alphabet.upcase
+  cesar = ""
+  text.chars.each_with_index do |letter,index|
+    if letter in 'a'..'z'
+      index = (alphabet.index(letter) + key)%26
+      cesar+=alphabet[index]
+    elsif letter in 'A'..'Z'
+      index = (alphabet_up.index(letter) + key)%26
+      cesar+=alphabet_up[index]
+    else cesar += letter
+    end
+  end
+  cesar
+end
+text = "What a string!"
+
+puts cesarean_code(text,5)
+
